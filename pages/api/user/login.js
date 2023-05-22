9b6d53f22b35f69b52aa4859fd1e93538296b9c0
Jhon Byron <jhoncolombia66@gmail.com>
2023-05-22 20:15:17 -0300

    Criado serviço de autenticação

---
+++ pages/api/user/login.js
@@ -0,0 +1,32 @@
+import Joi from "joi";
+import { withIronSessionApiRoute } from 'iron-session/next'
+
+import createHandler from '../../../lib/middlewares/nextConnect'
+import validate from '../../../lib/middlewares/validation'
+import { login } from "../../../modules/user/user.service"
+
+import { ironConfig } from '../../../lib/middlewares/ironSession'
+
+const LoginSchema = Joi.object({
+  userOrEmail: Joi.string().required(),
+  password: Joi.string().required()
+})
+
+const handler = createHandler()
+
+handler.post(validate({ body: LoginSchema }), async (req, res) => {
+  try {
+    const user = await login(req.body)
+    req.session.user = {
+      id: user._id,
+      user: user.user
+    }
+    await req.session.save()
+    res.send({ ok: true })
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+})
+
+export default withIronSessionApiRoute(handler, ironConfig)
