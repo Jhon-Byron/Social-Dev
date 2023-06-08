fb408d756205e27555a100b7415c29f535cca18b
Jhon Byron <jhoncolombia66@gmail.com>
2023-06-07 21:43:04 -0300

    Criado menu para posts

--- src/components/cards/Post.jsx
+++ src/components/cards/Post.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import moment from "moment"
 
+import Menu from "../navigation/Menu"
+
 const PostContainer = styled.div`
   background-color: ${props => props.theme.white};
   padding: 20px;
@@ -20,9 +22,36 @@ const ContainerText = styled.div`
   margin-top: 20px;
 `
 
+const ContainerMenu = styled.div`
+  float: right;
+`
+
+
 function Post ({ user, date, text }) {
+  const handleEdit = () => {
+    console.log("editar post")
+  }
+
+  const handleDelete = () => {
+    console.log("deletar post")
+  }
+
   return (
     <PostContainer>
+      <ContainerMenu>
+        <Menu 
+          options={[
+            {
+              text: 'editar publicação',
+              onClick: handleEdit
+            },
+            {
+              text: 'deletar publicação',
+              onClick: handleDelete
+            }
+          ]}
+        />
+      </ContainerMenu>
       <StyledUsername>@{user}</StyledUsername>
       <StyledDate>{moment(date).format('LLL')}</StyledDate>
       <ContainerText>
