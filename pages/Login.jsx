91b617e51bbc92c0c2bc6c2cd8e2e3b5d33650e6
Jhon Byron <jhoncolombia66@gmail.com>
2023-06-14 19:40:45 -0300

    Criado estado de carregando no botao

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -66,7 +66,7 @@ function LoginPage () {
           <Form onSubmit={handleSubmit(onSubmit)}>
             <Input label="Email ou Usuario" name="userOrEmail" control={control} />
             <Input label="Senha" type="password" name="password" control={control} />
-            <Button type="submit" disabled={Object.keys(errors).length > 0}>Entrar</Button>
+            <Button loading type="submit" disabled={Object.keys(errors).length > 0}>Entrar</Button>
           </Form>
           <Text>Não possui uma conta? <Link href="/signup">Faça seu cadastro</Link></Text>
         </FormContainer>
--- src/components/inputs/Button.jsx
+++ src/components/inputs/Button.jsx
@@ -1,6 +1,7 @@
+import { Children } from "react"
 import styled from "styled-components"
 
-const Button = styled.button`
+const StyledButton = styled.button`
   background-color: ${props => props.theme.primary};
   padding: 15px 20px;
   border-radius: 15px;
@@ -20,6 +21,16 @@ const Button = styled.button`
     background-color: ${props => props.theme.disabled};
   }
 `
-
+const Button = ({ children, loading, disabled, ...props }) =>{
+  return (
+    <StyledButton 
+      disabled={disabled || loading}
+      {...props}
+    >
+      {loading && <img src="./loading.svg" width="14px" />}
+      {!loading && children}
+    </StyledButton>
+  )
+}
 
 export default Button
